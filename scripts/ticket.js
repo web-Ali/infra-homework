4f9529d565379116a39925e483bc026c90f6ffd8
web-Ali <38067660+web-Ali@users.noreply.github.com>
2022-11-07 06:19:59 +0300

    fix-ticket

--- scripts/ticket.js
+++ scripts/ticket.js
@@ -3,15 +3,21 @@ const {api} = require("./api");
 
 async function updateTicket() {
     try {
+        console.log('Получаем последние коммиты')
         const {commits, lastCommiter} = await getCommits();
         console.log('Список последних коммитов')
         console.log(commits)
+        console.log('Автор последнего коммита')
         console.log(lastCommiter)
+
+        console.log('Получаем последний тег')
         const {lastTag} = await getTags();
         console.log(lastTag)
         const date = new Date();
         const title = `Релиз ${lastTag} - ${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
+        console.log(`Формируем заголовок: ${title}`)
         const description = `Ответственный за релиз: ${lastCommiter}\n\nКоммиты, попавшие в релиз:\n${commits}`;
+        console.log(`Формируем описание: ${description}`)
 
         console.log('Обновляем тикет')
         api.patchTicket({title, description})
