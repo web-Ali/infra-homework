8211060a1393926dfe3a301303af1377bdb75193
web-Ali <38067660+web-Ali@users.noreply.github.com>
2022-11-07 05:32:20 +0300

    fix_api

--- scripts/api.js
+++ scripts/api.js
@@ -1,15 +1,22 @@
 const axios =  require("axios");
 
-const instance = axios.create({
+const instanceTracker = axios.create({
     baseURL: 'https://api.tracker.yandex.net',
     headers: {
         'Authorization': 'OAuth y0_AgAAAAA2_F4IAAiPagAAAADTKOiW4Z8_AeU4Qqm3bVTh30USRAOjuYw',
         'X-Org-ID': '52232defb5db44119cd5f5073ed68339'
     }
 });
-
+const instanceGithub = axios.create({
+    baseURL: 'https://api.github.com',
+});
 const api = {
-    patchTicket: (data)=> instance.patch(`/v2/issues/${ticketId}`,JSON.stringify(data)),
-    postComment:(text)=> instance.post(`/v2/issues/comments`,JSON.stringify(text))
+    patchTicket: (data)=> instanceTracker.patch(`/v2/issues/${ticketId}`,JSON.stringify(data)),
+    postComment:(text)=> instanceTracker.post(`/v2/issues/comments`,JSON.stringify(text)),
+    getTags: ()=> instanceGithub.get('/repos/web-Ali/infra-homework/git/refs/tags').then(res=>res.data),
+    getRangeCommit: (lastTag, tagBeforeLast)=> instanceGithub.get(`/repos/web-Ali/infra-homework/compare/${tagBeforeLast}...${lastTag}`).then(res=>res.data),
+    getAllCommit: ()=> instanceGithub.get('/https://api.github.com/repos/web-Ali/infra-homework/commits').then(res=>res.data)
 }
-
+module.exports = {
+    api
+}
---
+++ scripts/utils.js
@@ -0,0 +1,40 @@
+let {api} = require("./api")
+
+async function getTags() {
+    try {
+        const data = await api.getTags();
+        if (data.length < 2) {
+            return {lastTag: '', tagBeforeLast: ''};
+        } else {
+            const regex = /rc-0.0.[0-9]+/;
+            let lastTag = regex.exec(data.pop().ref)[0];
+            let tagBeforeLast = regex.exec(data.pop().ref)[0];
+            return {lastTag, tagBeforeLast};
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getCommits() {
+    try {
+        const {lastTag, tagBeforeLast} = await getTags();
+        if (lastTag && tagBeforeLast) {
+            const data = await api.getRangeCommit(lastTag, tagBeforeLast);
+            const commits = data.commits.map((el) => {
+                return `${el.sha} ${el.commit.author.name} ${el.commit.message}`;
+            }).join('\n');
+            return commits;
+        } else {
+            const data = await api.getAllCommit();
+            const commits = data.map((el) => {
+                return `${el.sha} ${el.commit.author.name} ${el.commit.message}`;
+            }).join('\n');
+            return commits;
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getCommits().then(res => console.log(res))
